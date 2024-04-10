0453eccb8552bca463584c7e5ba942cdec60a5b3
Amaimono <k023g0048@g.neec.ac.jp>
2024-04-10 19:38:12 +0900

    _07_ex2

--- DirectXGame/scene/Enemy/Enemy.cpp
+++ DirectXGame/scene/Enemy/Enemy.cpp
@@ -3,48 +3,50 @@
 #include "TextureManager.h"
 #include <ImGuiManager.h>
 
-void (Enemy::*Enemy::updatesTable[])() = {&Enemy::ApproachUpdate, &Enemy::LeaveUpdate};
-
+#pragma region "Enemy"
 void Enemy::Init(const Vector3& pos) {
 	model_.reset(Model::Create());
 
+	state_.reset(new EnemyStateApproach(this));
+
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = pos;
-
-	phase_ = Phase::Approach;
 
 	th_ = TextureManager::Load("cute_cat.png");
 }
 
 void Enemy::Update() {
-	(this->*updatesTable[static_cast<size_t>(phase_)])();
+	// 現在のステートで更新処理
+	if (state_ != nullptr) {
+		state_->Update();
+	}
 
 	worldTransform_.UpdateMatrix();
-
-#ifdef _DEBUG
-	ImGui::Begin("Enemy");
-	ImGui::Text("Position :\nX %.2f \n Y %.2f \n Z %.2f", worldTransform_.translation_.x, worldTransform_.translation_.y, worldTransform_.translation_.z);
-	ImGui::Text("Current Phase : %s", phase_ == Phase::Approach ? "Approach" : "Leave");
-	ImGui::End();
-#endif // _DEBUG
 }
 
 void Enemy::Draw(const ViewProjection& viewProj) { model_->Draw(worldTransform_, viewProj, th_); }
 
-void Enemy::ApproachUpdate() {
+void Enemy::ChangeState(BaseEnemyState* nextState) { state_.reset(nextState); }
+
+#pragma endregion
+
+#pragma region "Enemy States"
+
+void EnemyStateApproach::Update() {
 	//========================================
 	//	移動
-	worldTransform_.translation_ += {0.0f, 0.0f, -kSpeed_};
+	host_->setPos(velocity + host_->getPos());
 
 	//========================================
-	if (worldTransform_.translation_.z <= 1.0f) {
-		phase_ = Phase::Leave;
+	if (host_->getPos().z <= 1.0f) {
+		// host は同じ
+		host_->ChangeState(new EnemyStateLeave(host_));
 	}
 }
 
-void Enemy::LeaveUpdate() {
+void EnemyStateLeave::Update() {
 	//========================================
 	//	移動
-	worldTransform_.translation_ += {kSpeed_, kSpeed_, -kSpeed_};
-	//========================================
+	host_->setPos(velocity + host_->getPos());
 }
+#pragma endregion
--- DirectXGame/scene/Enemy/Enemy.h
+++ DirectXGame/scene/Enemy/Enemy.h
@@ -8,9 +8,39 @@
 
 #include <memory>
 
-enum class Phase {
-	Approach,
-	Leave
+enum class Phase { Approach, Leave };
+class Enemy;
+class BaseEnemyState {
+public:
+	BaseEnemyState(Enemy* host) : host_(host){};
+	virtual ~BaseEnemyState(){};
+
+	virtual void Update() = 0;
+
+protected:
+	Enemy* host_;
+};
+
+class EnemyStateApproach : public BaseEnemyState {
+public:
+	EnemyStateApproach(Enemy* host) : BaseEnemyState(host){};
+	~EnemyStateApproach() override{};
+
+	void Update() override;
+
+private:
+	Vector3 velocity = {0.0f, 0.0f, -0.2f};
+};
+
+class EnemyStateLeave : public BaseEnemyState {
+public:
+	EnemyStateLeave(Enemy* host) : BaseEnemyState(host){};
+	~EnemyStateLeave() override{};
+
+	void Update() override;
+
+private:
+	Vector3 velocity = {-0.5f, 0.5f, 0.0f};
 };
 
 class Enemy {
@@ -18,12 +48,8 @@ public:
 	void Init(const Vector3& pos);
 	void Update();
 	void Draw(const ViewProjection& viewProj);
-	
-private:
-	static void (Enemy::*updatesTable[])();
 
-	void ApproachUpdate();
-	void LeaveUpdate();
+	void ChangeState(BaseEnemyState* nextState);
 
 private:
 	WorldTransform worldTransform_;
@@ -31,7 +57,12 @@ private:
 
 	std::unique_ptr<Model> model_ = nullptr;
 
-	Phase phase_ = Phase::Approach;
+	std::unique_ptr<BaseEnemyState> state_;
 
 	uint32_t th_;
+
+public:
+	const Vector3& getPos() const { return worldTransform_.translation_; }
+	void setPos(const Vector3& pos) { worldTransform_.translation_ = pos; }
 };
+
